3269781bae4c3e94af399d3682c47675048acf47
yukuda <leeyookoo@naver.com>
2021-09-04 17:29:16 +0900

    User

--- photogram/src/main/java/com/cos/photogramstart/domain/user/User.java
+++ photogram/src/main/java/com/cos/photogramstart/domain/user/User.java
@@ -18,30 +18,32 @@ import lombok.NoArgsConstructor;
 
 @Builder
 @AllArgsConstructor // 전체생성자
-@NoArgsConstructor	// 빈생성자
+@NoArgsConstructor // 빈생성자
 @Data
 @Entity // 디비에 테이블을 생성
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // 번호 증가 전략이 데이터베이스를 따라간다.
 	private int id;
-	
-	@Column(unique = true)
+
+	@Column(length = 20, unique = true)
 	private String username;
+	@Column(nullable = false)
 	private String password;
-	
+	@Column(nullable = false)
 	private String name;
 	private String website;
-	private String bio;	// 자기소개
+	private String bio; // 자기소개
+	@Column(nullable = false)
 	private String email;
 	private String phone;
 	private String gender;
-	
+
 	private String profileImageUrl; // 사진
 	private String role; // 권한
-	
+
 	private LocalDateTime createDate;
-	
+
 	@PrePersist // 디비에 INSERT가 되기 직전에 실행
 	public void createDate() {
 		this.createDate = LocalDateTime.now();
