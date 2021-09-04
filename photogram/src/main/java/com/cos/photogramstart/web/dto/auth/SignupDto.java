c0f9a9348e983e21c0217443633a07a3421ab5d2
yukuda <leeyookoo@naver.com>
2021-09-04 17:29:35 +0900

    SignupDto

--- photogram/src/main/java/com/cos/photogramstart/web/dto/auth/SignupDto.java
+++ photogram/src/main/java/com/cos/photogramstart/web/dto/auth/SignupDto.java
@@ -1,5 +1,9 @@
 package com.cos.photogramstart.web.dto.auth;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 import com.cos.photogramstart.domain.user.User;
 
 import lombok.Data;
@@ -7,9 +11,14 @@ import lombok.Data;
 // 통신을 할때 담아두는 오브젝트 : Dto
 @Data // Getter, Setter
 public class SignupDto {
+	@Size(min=2,max=20)
+	@NotBlank
 	private String username;
+	@NotBlank
 	private String password;
+	@NotBlank
 	private String email;
+	@NotBlank
 	private String name;
 	
 	public User toEntity() {
