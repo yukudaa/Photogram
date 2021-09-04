f3fcd59b6918f8e4ef2615ae2265e6bd1f147651
yukuda <leeyookoo@naver.com>
2021-09-04 17:29:24 +0900

    AuthController

--- photogram/src/main/java/com/cos/photogramstart/web/AuthController.java
+++ photogram/src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -1,12 +1,21 @@
 package com.cos.photogramstart.web;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.handler.ex.CustomValidationException;
 import com.cos.photogramstart.service.AuthService;
 import com.cos.photogramstart.web.dto.auth.SignupDto;
 
@@ -16,33 +25,45 @@ import lombok.RequiredArgsConstructor;
 @Controller // 1. IoC 2. 파일을 리턴하는 컨트롤러
 // 회원가입
 public class AuthController {
-	
+
 	private static final Logger log = LoggerFactory.getLogger(AuthController.class);
 
 	private final AuthService authService;
-	
+
 //	public AuthController(AuthService authService) {
 //		this.authService = authService;
 //	}
-	
+
 	@GetMapping("/auth/signin")
 	public String signinForm() {
 		return "auth/sighin";
 	}
-	
+
 	@GetMapping("/auth/signup")
 	public String signupForm() {
 		return "auth/sighup";
 	}
-	
+
 	// 회원가입버튼 -> /auth/sighup -> /auth/signin
+	// 회원가입버튼 X
 	@PostMapping("/auth/signup")
-	public String signup(SignupDto signupDto) { // key=value (x-www-form-urlencoded)
-		log.info(signupDto.toString());
-		// User <- SignupDto
-		User user = signupDto.toEntity();
-		User userEntity = authService.회원가입(user);
-		System.out.println(userEntity);
-		return "auth/sighin";
+	public String signup(@Valid SignupDto signupDto, BindingResult bindingResult) { // key=value
+																									// (x-www-form-urlencoded)
+		if (bindingResult.hasErrors()) {
+			Map<String, String> errorMap = new HashMap<>();
+
+			for (FieldError error : bindingResult.getFieldErrors()) {
+				errorMap.put(error.getField(), error.getDefaultMessage());
+			}
+			throw new CustomValidationException("유효성검사 실패함",errorMap);
+		} else {
+			// log.info(signupDto.toString());
+			// User <- SignupDto
+			User user = signupDto.toEntity();
+			User userEntity = authService.회원가입(user);
+			System.out.println(userEntity);
+			return "auth/sighin";
+		}
+
 	}
 }
