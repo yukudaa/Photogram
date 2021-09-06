87a26736cc51ccd4c8be879d8e63e0a8f2abb2f9
yukuda <leeyookoo@naver.com>
2021-09-06 15:54:40 +0900

    userController

---
+++ photogram/src/main/java/com/cos/photogramstart/web/userController.java
@@ -0,0 +1,22 @@
+package com.cos.photogramstart.web;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.cos.photogramstart.config.auth.PrincipalDetails;
+
+@Controller
+public class userController {
+	
+	@GetMapping("/user/{id}")
+	public String profile(@PathVariable int id) {
+		return "user/profile";
+	}
+	
+	@GetMapping("/user/{id}/update")
+	public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalDetails principalDeatails) {
+		return "user/update";
+	}
+}
