83576fab462380f6aaeac41464671d3d4f652e1a
yukuda <leeyookoo@naver.com>
2021-09-06 15:54:34 +0900

    ImageController

---
+++ photogram/src/main/java/com/cos/photogramstart/web/ImageController.java
@@ -0,0 +1,25 @@
+package com.cos.photogramstart.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ImageController {
+
+	@GetMapping({ "/", "/image/stroy" })
+	public String story() {
+		return "image/story";
+	}
+	
+	@GetMapping("/image/popular")
+	public String popular() {
+		return "image/popular";
+	}
+	
+	@GetMapping("/image/upload")
+	public String upload() {
+		return "image/upload";
+	}
+	
+
+}
