065e6456bc52162332468b46b24ab573e69e85fa
yukuda <leeyookoo@naver.com>
2021-09-09 16:14:59 +0900

    CustomApiException

---
+++ photogram/src/main/java/com/cos/photogramstart/handler/ex/CustomApiException.java
@@ -0,0 +1,13 @@
+package com.cos.photogramstart.handler.ex;
+
+import java.util.Map;
+
+public class CustomApiException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public CustomApiException(String message) {
+		super(message);
+	}
+
+}
