1114678f614a0c2e33a8056b4538ff6b4139b839
yukuda <leeyookoo@naver.com>
2021-09-15 00:14:47 +0900

    ImageController

--- photogram/src/main/java/com/cos/photogramstart/web/ImageController.java
+++ photogram/src/main/java/com/cos/photogramstart/web/ImageController.java
@@ -1,25 +1,42 @@
 package com.cos.photogramstart.web;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.cos.photogramstart.config.auth.PrincipalDetails;
+import com.cos.photogramstart.service.ImageService;
+import com.cos.photogramstart.web.dto.image.ImageUploadDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Controller
 public class ImageController {
+
+	private final ImageService imageService;
 
 	@GetMapping({ "/", "/image/stroy" })
 	public String story() {
 		return "image/story";
 	}
-	
+
 	@GetMapping("/image/popular")
 	public String popular() {
 		return "image/popular";
 	}
-	
+
 	@GetMapping("/image/upload")
 	public String upload() {
 		return "image/upload";
 	}
-	
+
+	@PostMapping("/image")
+	public String imageUpload(ImageUploadDto imageUploadDto, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+		// 서비스 호출
+		imageService.사진업로드(imageUploadDto, principalDetails);
+		return "redirect:/user/" + principalDetails.getUser().getId();
+	}
 
 }
